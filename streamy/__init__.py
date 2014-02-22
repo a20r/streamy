d7e90104078b101433494070c914e85cfae3de8c
Alexander James Wallar <aw204@st-andrews.ac.uk>
2014-02-22 23:49:26 +0000

    urghh

--- streamy/__init__.py
+++ streamy/__init__.py
@@ -3,11 +3,10 @@
 def run(host, port):
     import server.config as config
     from flask import Flask
-    from geventwebsocket.handler import WebSocketHandler
     from gevent.pywsgi import WSGIServer
 
     http_server = WSGIServer(
-        (host, port), config.app, handler_class=WebSocketHandler
+        (host, port), config.app
     )
 
     http_server.serve_forever()
