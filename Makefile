11f4d58a06ea0f2c1a78de5dcddf263fe08a5127
Alexander James Wallar <aw204@st-andrews.ac.uk>
2014-02-22 15:14:45 +0000

    Add depend make

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 
-all : *.py
+all: *.py depend
 	@python run.py
+
+depend: requirements.txt
+	@sudo pip install -r requirements.txt
+
--- streamy/server/pageserver.py
+++ streamy/server/pageserver.py
@@ -1,11 +1,31 @@
 
-from flask import Flask
-from flask import render_template
 
+from flask import request, redirect, url_for, abort, jsonify, render_template
+from flask import make_response, Response
 import config
 
+""" Directory for static files """
+STATIC_DIR = "assets/"
 
-@config.app.route('/')
-def index():
-    return render_template('index.html', title="home")
+""" Mime type dictionary """
+MIME_DICT = {
+    "js": "text/javascript",
+    "css": "text/css",
+    "img": "image/png",
+    "libraries": "text/javascript",
+    "data": "text/csv"
+}
+
+@config.app.route("/<filename>", methods = ["GET"])
+def serve_html_page(filename):
+    if filename == "favicon.ico":
+        with open(STATIC_DIR + "img/favicon.ico") as f:
+            return Response(f.read(), mimetype = "image/x-icon")
+
+    return render_template(filename)
+
+@config.app.route("/<filetype>/<filename>", methods = ["GET"])
+def serve_script(filetype, filename):
+    with open(STATIC_DIR + filetype + "/" + filename) as f:
+        return Response(f.read(), mimetype = MIME_DICT[filetype])
 
